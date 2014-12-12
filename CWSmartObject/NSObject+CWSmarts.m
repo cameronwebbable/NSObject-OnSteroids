7d3629330dc6525cafb8ab7fb5232877a49e6e0c
Cameron Webb <cwebb02@gmail.com>
2014-12-12 13:59:20 -0600

    first commit

---
+++ CWSmartObject/NSObject+CWSmarts.m
@@ -0,0 +1,19 @@
+//
+//  NSObject+CWSmarts.m
+//  CWSmartObject
+//
+//  Created by Cameron Webb on 12/12/14.
+//  Copyright (c) 2014 Cameron Webb. All rights reserved.
+//
+
+#import "NSObject+CWSmarts.h"
+
+@interface NSObject ()
+-(instancetype) mapObject:(id)object withValues:(NSDictionary *)dictionary;
+@end
+
+@implementation NSObject
+
+
+
+@end
