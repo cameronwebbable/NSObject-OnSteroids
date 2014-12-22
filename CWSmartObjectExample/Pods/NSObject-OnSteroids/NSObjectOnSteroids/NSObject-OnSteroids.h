e70914da57ea6df255cd96bba82ddeb9c71b9980
Cameron Webb <cwebb02@gmail.com>
2014-12-21 19:37:52 -0600

    Gah

--- CWSmartObjectExample/Pods/NSObject-OnSteroids/NSObjectOnSteroids/NSObject-OnSteroids.h
+++ CWSmartObjectExample/Pods/NSObject-OnSteroids/NSObjectOnSteroids/NSObject-OnSteroids.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char CWSmartObjectVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CWSmartObject/PublicHeader.h>
 
-#import <CWSmartObject/NSObject+CWSmarts.h>
+#import <NSObjectOnSteroids/NSObject+CWSmarts.h>
